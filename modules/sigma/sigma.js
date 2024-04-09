e977a80dcd6aaa61e0bd77f3b2cb4dcb81f28b97
DrunkMule <71262916+DrunkMule@users.noreply.github.com>
2024-04-09 12:21:44 +0200

    Update sigma.js

--- modules/sigma/sigma.js
+++ modules/sigma/sigma.js
@@ -5,7 +5,7 @@ module.exports = {
     type: 'command',
     data: new SlashCommandBuilder()
         .setName('erm')
-        .setDescription('Replies with childpo when called'),
+        .setDescription('Erm what the sigma?'),
     async execute(interaction) {
         await interaction.reply(
             'https://tenor.com/view/erm-what-the-sigma-squidward-meme-tiktok-reaction-gif-574741893352575449'
